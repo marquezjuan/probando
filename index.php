5ca0e40316c55f4046d08476a6ec0b541e014096
marquezjuan <juanse.marquez+gh@gmail.com>
2019-03-30 13:25:29 -0300

    Commit inicial de prueba

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PROBANDO</title>
+    </head>
+    <body>
+        <h1>Probando php</h1>
+        <?php
+              phpinfo();
+        ?>
+    </body>
+</html>
